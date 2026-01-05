ee7fcf410eb0644f692c829b35b517628997aa29
Nara Avila <nara.moraes@usp.br>
2026-01-04 23:47:01 -0300

    Update files

--- scripts/version_control/rel.sh
+++ scripts/version_control/rel.sh
@@ -7,39 +7,39 @@ rel() {
   shift || true
 
   case "$cmd" in
-    wipe)
-      case "$sub" in
-        releases)
-          rel_wipe_releases
-          ;;
-        tags)
-          rel_wipe_tags
-          ;;
-        all)
-          rel_wipe_all
-          ;;
-        project)
-          rel_wipe_project
-          ;;
-        *)
-          echo "Uso:"
-          echo "  rel wipe project"
-          echo "  rel wipe releases"
-          echo "  rel wipe tags"
-          echo "  rel wipe all"
-          return 1
-          ;;
-      esac
-      return 0
-      ;;
+    # wipe)
+    #   case "$sub" in
+    #     releases)
+    #       rel_wipe_releases
+    #       ;;
+    #     tags)
+    #       rel_wipe_tags
+    #       ;;
+    #     all)
+    #       rel_wipe_all
+    #       ;;
+    #     project)
+    #       rel_wipe_project
+    #       ;;
+    #     *)
+    #       echo "Use:"
+    #       echo "  rel wipe project"
+    #       echo "  rel wipe releases"
+    #       echo "  rel wipe tags"
+    #       echo "  rel wipe all"
+    #       return 1
+    #       ;;
+    #   esac
+    #   return 0
+    #   ;;
     init) rel_init "$@" ;;
     p)    rel_patch "$@" ;;
     m)    rel_minor "$@" ;;
     M)    rel_major "$@" ;;
     *)
-      echo "Uso:"
+      echo "Use:"
       echo "  rel init Item 1 -- Item 2 -- Item 3"
-      echo "  rel p #<issue>"
+      echo "  rel p <#issue>"
       echo "  rel m"
       echo "  rel M"
       return 1
